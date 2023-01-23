bee5cfab7665f19dec1c52ef6082625cc97853d5
gauravv0312 <gauravv0312@gmail.com>
2023-01-23 17:55:52 +0530

    post creation page

---
+++ client/src/PostCreate.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { async } from 'q';
+import React from 'react'
+import { useState } from 'react'
+function PostCreate() {
+    const [title,setTitle] = useState('');
+
+    const onSubmit = async(e)=>{
+       e.preventDefault();
+       await axios.post('http://localhost:4000/posts',{title});
+       setTitle('');
+    }
+  return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <div className='form-group'>
+                <label>Title</label>
+                <input value={title} onChange={e=> setTitle(e.target.value)} className='form-control'/>
+            </div>
+            <button className='btn btn-primary'>Submit</button>
+        </form>
+        {console.log(title)}
+    </div>
+  )
+}
+
+export default PostCreate
